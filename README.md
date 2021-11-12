43a60a7daec8fbbe407dcace119519ee478de353
Jeremy Ward <jeremy.ward@shopify.com>
2021-11-12 15:55:47 -0500

    Spin it up

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-dotfiles
-========
+# dotfiles
+
 using github.com/thoughtbots/dotfiles & github.com/thoughtbots/rcm
 
 1. git clone --recursive git@github.com:basicBrogrammer/dotfiles.git
-2. In the terminal run `$HOME/dotfiles/init.sh`
+2. In the terminal run `$HOME/dotfiles/install.sh`
 
 Then, open vim and run `:PlugInstall`
---
+++ install.sh
@@ -0,0 +1,34 @@
+echo 'Downloading thoughtbot/laptop....'
+
+cd $HOME
+# if macOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  curl --remote-name https://raw.githubusercontent.com/thoughtbot/laptop/master/mac
+
+  if [ ! -L "$HOME/.laptop.local" ]; then
+      echo "Symlinking .laptop.local..."
+      ln -s  "$HOME/dotfiles/laptop.local" "$HOME/.laptop.local"
+  else
+      echo ".laptop.local already linked..."
+  fi
+
+  echo 'Running `sh mac 2>&1 | tee ~/laptop.log`...'
+
+  sh mac 2>&1 | tee ~/laptop.log
+else # Linux basic setup
+  # install ohmyzsh
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  npm install --global pure
+
+  # install RCM
+  sudo apt-get install software-properties-common
+  sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
+  sudo apt-get update
+  sudo apt-get install rcm
+
+  export RCRC=$HOME/dotfiles/rcrc
+
+  # NOTE: this hasn't been tested
+  echo 'linking rc files'
+  rcup
+fi
