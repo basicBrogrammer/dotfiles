876a7095756d88b9292d86106a79d344c4d60bc3
Jeremy Ward <jeremy.ward@shopify.com>
2024-05-24 14:55:38 -0400

    figured out how to edit lazyvim bindings lfg

---
+++ config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,20 @@
+local have_make = vim.fn.executable("make") == 1
+local have_cmake = vim.fn.executable("cmake") == 1
+
+return {
+  -- file explorer
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    cmd = "Neotree",
+    keys = {
+      {
+        "<leader>ft",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+      },
+    },
+  },
+}
