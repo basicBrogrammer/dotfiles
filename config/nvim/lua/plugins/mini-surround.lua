2d179196858df9c5ec25ee1b2385072e6bc7cc53
basicbrogrammer <jcward10@gmail.com>
2024-05-28 22:49:35 -0400

    update surround keys

---
+++ config/nvim/lua/plugins/mini-surround.lua
@@ -0,0 +1,9 @@
+return {
+  "echasnovski/mini.surround",
+  opts = {
+    mappings = {
+      add = "S", -- Add surrounding in Normal and Visual modes
+      replace = "cs", -- Replace surrounding
+    },
+  },
+}
